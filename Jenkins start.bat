afa3640a03f3ad824d76d6bf3b22e47813ff835e
Nadav Chen <71144691+Nadav23AnT@users.noreply.github.com>
2020-10-07 16:58:48 +0300

    Added comments

--- Jenkins start.bat
+++ Jenkins start.bat
@@ -1,5 +1,7 @@
 @ECHO OFF
+# cd to Where Jenkins client is located in your PC
 cd C:\Users\Nadav Chen\Documents\jen
+# open the jenkins web client with your default browser [LINK: Jenkins URL] 
 start "" http://localhost:8080/
 java -jar jenkins.war
 PAUSE
